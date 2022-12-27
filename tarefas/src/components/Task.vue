7fc5ab5f75ea32821b675ac61a31672128804633
Galileo Guilherme <galileoguilherme@outlook.com>
2022-12-27 16:38:18 -0300

    Update Task.vue

--- tarefas/src/components/Task.vue
+++ tarefas/src/components/Task.vue
@@ -1,16 +1,27 @@
 <template>
     <div id="task">
-        <form @submit.prevent="addItem">
-            <input type="text" placeholder="Tarefa de hoje" v-model="tarefa"/>
-            <button type="submit">Adicionar</button>
-        </form>
-        <Item :lista="tarefas" :delete="deleteTask"/>
+        
+       <form @submit.prevent="addItem" >
+           <input
+           type="text"
+           placeholder="Tarefa de hoje?"
+           v-model="tarefa"
+           />
+           <button type="submit">Adicionar</button>
+       </form>
+
+       <Item :lista="tarefas" :delete="deleteTask" />
+
+       <span v-show="tarefas.length > 0">
+           Você tem <strong :class="{pend: pendente}" >{{tarefas.length}}</strong> tarefas pendentes.
+       </span>
+
     </div>
 </template>
 
 
 <script>
-import Item from './Item'
+import Item from './Item';
 export default {
     name: 'Task-',
     components:{
@@ -20,43 +31,45 @@ export default {
         return{
             tarefa: '',
             tarefas: [],
+            pendente: false
         }
     },
     methods:{
         addItem(){
-            if(this.tarefa !== ''){
-                this.tarefas.push({
-                    text: this.tarefa,
-                    key: Date.now(),
-                });
-            }else{
-                alert('Digite alguma tarefa..');
-                return;
-            }
+           if(this.tarefa !== ''){
+               this.tarefas.push({
+                   text: this.tarefa,
+                   key: Date.now(),
+               });
+           }else{
+               alert('Digite uma tarefa..');
+               return;
+           }
 
             this.tarefa = '';
+            console.log(this.tarefas);
+
         },
         deleteTask(key){
-            let filtro = this.tarefas.filter((item) => {
-                return (item.key !== key);
-            });
+           let filtro = this.tarefas.filter( (item) => {
+              return (item.key !== key);
+           });
 
-            this.tarefas = filtro;
-        },
-        watch:{
-            tarefas:{
-                deep: true,
-                handler(){
-                localStorage.setItem('tasks', JSON.stringify(this.tarefas));
-
-            }
-            }
-        },
-        created() {
-            const minhaLista = localStorage.getItem('tasks');
-            this.tarefas = JSON.parse(minhaLista) || [];
+           return this.tarefas = filtro;
         }
-            
+    },
+    watch:{
+        tarefas:{
+            deep: true,
+            handler(){
+                localStorage.setItem('tasks', JSON.stringify(this.tarefas));
+                this.tarefas.length > 4 ? this.pendente = true : this.pendente = false;             
+            }
+        }
+    },
+    created(){ // É chamado automaticamente após a instancia do vuejs ser criada..
+        const minhaLista = localStorage.getItem('tasks');
+        this.tarefas = JSON.parse(minhaLista) || [];
     }
 }
 </script>
@@ -64,39 +77,42 @@ export default {
 <style scoped>
     #task{
         max-width: 700px;
-        background: white;
+        background: #FFF;
         border-radius: 4px;
         padding: 20px;
         margin: 20px auto;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);        
+        box-shadow: 0 0 20px rgba(0,0,0, 0.3);
     }
+
     form{
         margin-top: 30px;
         display: flex;
         flex-direction: row;
-
     }
+
     form button{
         cursor: pointer;
         background: #0f5959;
-        border: 0;
+        border:0;
         border-radius: 4px;
         margin-left: 10px;
         padding: 0 15px;
         display: flex;
         justify-content: center;
         align-items: center;
-        color: #fff;
+        color: #FFF;
     }
+
     input{
-        flex: 1;
-        border: 1px solid #eee;
-        padding: 6px 10px;
-        border-radius: 4px;
-        font-size: 14px;
-        outline: none;
+      flex:1;  
+      border: 1px solid #eee;
+      padding: 6px 10px;
+      border-radius: 4px;
+      font-size: 14px;
+      outline: none;
     }
 
+    .pend{
+       color: #FF0000; 
+    }
 </style>
-
-
